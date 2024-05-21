fc85c0a581cc96e834aab9a5d461b9aebdba4b67
Michael Caballero <mcaballero.dev@gmail.com>
2024-05-20 21:50:54 -0700

    Move example files into their own directory

---
+++ examples/receiver.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+def get_file_info(url, file_path):
+    # setup request msg
+    request_dict = {'file_path': file_path}
+    
+    # send request and save response 
+    response = requests.post(url, json=request_dict)
+    
+    # if the response is OK
+    if response.status_code == 200:
+        # Print the JSON response
+        print(json.dumps(response.json(), indent=4))
+    else:
+        print(f"Error retrieving file info. Status: {response.status_code}\nResponse: {response.text}")
+
+if __name__ == "__main__":
+    server_url = "http://localhost:5000/fileinfo"
+    
+    file_path = "/home/mcaballero/Development/CS361/fileinfo_microservice/examples/larbs.sh"
+    
+    # request file and print the JSON response
+    get_file_info(server_url, file_path)
+
